8b6d936d66a8b610c2eb0f2c887f9ab1eef1f51d
Gichuki-Den <dennisg.mwangi@gmail.com>
2020-01-13 21:01:55 +0300

    first commit

---
+++ string.py
@@ -0,0 +1,16 @@
+
+
+#string slicing
+newVar="Programming"
+
+slicedChars= newVar[-4:-8] #wrong
+slicedChars= newVar[-8:-4]
+slicedChar= newVar[3:7]
+print(slicedChars)
+print(slicedChar)
+
+str1="mwai"
+str2="Kibaki"
+
+fullName=str1+" "+str2
+print(fullName)
---
+++ variables.py
@@ -0,0 +1,13 @@
+#Variable is a named storage location
+
+""""
+This is a multi line comment
+"""
+
+#create a variable to hold my name
+myName="Dennis"
+print(myName)
+myAge=30
+print(type(myName))
+print(type(myAge))
+
